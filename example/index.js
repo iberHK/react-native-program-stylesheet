cbce086e1e824e70e47ad4e5debd6d15a66cb408
iberHK <chenjunjin@chenjunjindeMacBook-Pro.local>
2018-03-23 05:00:05 +0800

    初始化

--- example/index.js
+++ example/index.js
@@ -2,6 +2,6 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 
 //需要在程序入口处导入
-import StyleSheet375 from 'react-native-program-stylesheet';
+import getSize from 'react-native-program-stylesheet';
 
 AppRegistry.registerComponent('example', () => App);
